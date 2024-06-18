bbebe19dc527b387e17a511c9002e6b06d1ea3df
magen <2023638563@qq.com>
2024-06-18 18:01:14 +0800

    数据注入界面（FaultDataView）新增节点信息表格（nodeInfoData），新增检测节点按钮，触发函数为detectNodes()

---
+++ front/data/NodeInfoColumn.js
@@ -0,0 +1,14 @@
+const NodeInfoColumn = [
+    { label: '节点号', prop: 'node', width: '200', key: '100' },
+    { label: '设备类型', prop: 'deviceType', width: '200', key: '200' ,},
+    { label: '网卡类型', prop: 'networkCard', width: '200', key: '300', },
+    { label: 'MAC', prop: 'MAC', width: '200', key: '400',  },
+    { label: 'IP', prop: 'IP', width: '200', key: '500', },
+    { label: '上行节点', prop: 'upstreamNode', width: '220', key: '600',  },
+];
+
+
+
+export {
+    NodeInfoColumn
+}
--- front/src/views/fault/FaultDataView.vue
+++ front/src/views/fault/FaultDataView.vue
@@ -1,35 +1,53 @@
 <template>
   <div>
-    <el-card>
-      <p style="text-align: center">数据注入界面</p>
-      <el-form :model="faultDataForm" ref="faultDataForm" label-width="100px" class="demo-ruleForm">
-        <el-form-item label="节点A" prop="nodeA">
-          <el-input v-model="faultDataForm.nodeA" autocomplete="off" style="width: 70%"></el-input>
-        </el-form-item>
-        <el-form-item label="节点B" prop="nodeB">
-          <el-input v-model="faultDataForm.nodeB" autocomplete="off" style="width: 70%"></el-input>
-        </el-form-item>
-        <el-form-item label="带宽" prop="nodeData">
-          <el-input v-model.number="faultDataForm.bandWidth" style="width: 70%"></el-input>
-          <span>bps</span>
-        </el-form-item>
-        <el-form-item label="发送时间" prop="nodeData">
-          <el-input v-model.number="faultDataForm.sendTime" style="width: 70%"></el-input>
-          <span>t</span>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="submitForm('faultDataForm')">提交</el-button>
-          <el-button type="warning" @click="scanNodes()">扫描节点</el-button>
-          <el-button @click="resetForm('faultDataForm')">重置</el-button>
-        </el-form-item>
-      </el-form>
-    </el-card>
+    <div>
+      <el-card>
+        <p style="text-align: center; color: black;margin-top: 0px; font-weight: bold; font-size: 20px">节点信息</p>
+        <el-button type="primary" @click="detectNodes()" style="float: right; margin-top: -10px; margin-bottom: 10px">检测节点</el-button>
+        <el-table :data="nodeInfoData" max-height="250" border stripe highlight-current-row>
+          <el-table-column
+              v-for="(item, index) in nodeInfoColumn"
+              :key="index"
+              :prop="item.prop"
+              :label="item.label"
+              :width="item.width">
+          </el-table-column>
+        </el-table>
+      </el-card>
+    </div>
+    <div style="margin-top: 10px">
+      <el-card>
+        <p style="text-align: center; color: black;margin-top: 0px; font-weight: bold; font-size: 20px">数据注入界面</p>
+        <el-form :model="faultDataForm" ref="faultDataForm" label-width="100px" class="demo-ruleForm">
+          <el-form-item label="节点A" prop="nodeA">
+            <el-input v-model="faultDataForm.nodeA" autocomplete="off" style="width: 70%"></el-input>
+          </el-form-item>
+          <el-form-item label="节点B" prop="nodeB">
+            <el-input v-model="faultDataForm.nodeB" autocomplete="off" style="width: 70%"></el-input>
+          </el-form-item>
+          <el-form-item label="带宽" prop="nodeData">
+            <el-input v-model.number="faultDataForm.bandWidth" style="width: 70%"></el-input>
+            <span>bps</span>
+          </el-form-item>
+          <el-form-item label="发送时间" prop="nodeData">
+            <el-input v-model.number="faultDataForm.sendTime" style="width: 70%"></el-input>
+            <span>t</span>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="submitForm('faultDataForm')">提交</el-button>
+            <el-button type="warning" @click="scanNodes()">扫描节点</el-button>
+            <el-button @click="resetForm('faultDataForm')">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </el-card>
+    </div>
   </div>
 </template>
 
 <script>
 
 import axios from "axios";
+import {NodeInfoColumn} from "../../../data/NodeInfoColumn";
 
 export default {
   name: "FaultDataView",
@@ -41,7 +59,8 @@ export default {
         bandWidth: '',
         sendTime: ''
       },
-
+      nodeInfoData: [],
+      nodeInfoColumn: NodeInfoColumn,
     }
   },
   mounted() {
@@ -50,6 +69,10 @@ export default {
   methods: {
     // 扫描接口
     scanNodes() {
+
+    },
+    // 检测节点触发函数
+    detectNodes() {
 
     },
     resetForm(formName) {

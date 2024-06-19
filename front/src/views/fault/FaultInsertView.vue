e20e2971dacf6dbc1b9246e978ca6f5b072f0c31
magen <2023638563@qq.com>
2024-06-19 11:04:33 +0800

    对故障注入界面的提交的表单faultDataInsertForm格式进行修改,简化传输的数据格式

--- front/src/views/fault/FaultInsertView.vue
+++ front/src/views/fault/FaultInsertView.vue
@@ -19,7 +19,7 @@
         </el-form-item>
         <el-form-item
             v-for="(node, index) in faultDataInsertForm.nodes"
-            :label="'节点' + index"
+            :label="'节点' + node.key"
             :key="node.key"
             :prop="'nodes.' + index + '.value'"
             :rules="{
@@ -70,7 +70,8 @@ export default {
       }],
       faultDataInsertForm: {
         nodes: [{
-          value: ''
+          value: '',
+          key: 'A'
         }],
         faultType: '',
         faultParameter: ''
@@ -89,6 +90,17 @@ export default {
     submitForm(formName) {
       this.$refs[formName].validate(async (valid) => {
         if (valid) {
+          // 数据格式转换
+          let newFaultDataInsertForm = {
+            "faultParameter": this.faultDataInsertForm.faultParameter,
+            "faultType": this.faultDataInsertForm.faultType
+          };
+          // 根据nodes的长度添加node$对象
+          for (let i = 0; i < this.faultDataInsertForm.nodes.length; i++) {
+            let node = this.faultDataInsertForm.nodes[i];
+            newFaultDataInsertForm[`node${node.key}`] = node.value;
+          }
+          // console.log(newFaultDataInsertForm);
           // 校验通过，注入故障
           const res = await axios.post("/api/data_insert", this.faultDataInsertForm, {});
           if (res.data.code === 200) {
@@ -105,15 +117,26 @@ export default {
       this.$refs[formName].resetFields();
     },
     removeNode(item) {
-      let index = this.faultDataInsertForm.nodes.indexOf(item)
-      if (index !== -1) {
-        this.faultDataInsertForm.nodes.splice(index, 1)
+      if (this.faultDataInsertForm.nodes.length === 1) {
+        this.$alert('至少需要一个节点', '提示', {
+          confirmButtonText: '确定',
+        });
+      } else {
+        let index = this.faultDataInsertForm.nodes.indexOf(item)
+        if (index !== -1) {
+          this.faultDataInsertForm.nodes.splice(index, 1)
+        }
       }
     },
     addNode() {
+      // 获取当前最后一个节点的键值
+      const lastKey = this.faultDataInsertForm.nodes[this.faultDataInsertForm.nodes.length - 1].key ;
+      // 根据最后一个节点的键值计算下一个键值
+      const nextKey = String.fromCharCode(lastKey.charCodeAt(0) + 1);
+      // 添加新节点并设置键值
       this.faultDataInsertForm.nodes.push({
         value: '',
-        key: Date.now()
+        key: nextKey
       });
     }
   },
